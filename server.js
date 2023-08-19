17a50e90302759d9ccc2b7a6c13f322ad113ed9d
Aaron Flores <aaron_flores08@yahoo.com>
2023-08-18 19:24:52 -0500

    structued files in app

--- server.js
+++ server.js
@@ -1,36 +1,16 @@
-// import express
-const express = require('express');
-const path = require('path');
-// import the feedback router
-const api = require('./');
-const notes = require('./Develop/db/db.json');
 
-const PORT = process.env.PORT || 3001;
-//  init express app.
+const express = require ('express');
+// reminder this is used to deal with file paths!
+const path = require ('path');
 const app = express();
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Middleware to serve up static assets from the public folder
-app.use(express.static('public'));
+// app.get('api/', (req,res)=> {
+// res.json()
 
-// Send all the requests that begin with /api to the index.js in the routes folder
-app.use('/api', api);
+// 1) Notes make the public folder static
+app.use(express.static(path.join(__dirname, 'public')));
 
+const PORT = process.env.PORT || 5002;
 
-// This view route is a GET route for the notes page
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-// This is the wildcard route to direct users to a 404 page
-app.get('*', (req, res) =>
-    res.sendFile(path.join (_dirname, './public/pages/index.html'))
-);
+app.listen(PORT, () => console.log (`Server started on http://localhost:${PORT}`));
 
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
-
-// app.listen(process.env.PORT || 3001);
