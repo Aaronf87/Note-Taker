fa89f13885c8c8c4979e4dd136e6269b11880f0a
Aaron Flores <97806359+Aaronf87@users.noreply.github.com>
2023-09-09 01:05:01 -0500

    restructured files

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const path = require ('path');
 // const notes = require ('public/notes.html')
 const app = express();
 
-app.get('db/db.json', (req,res)=> res.json(db));
 
-// 1) Notes make the public folder static
+
+
+// 1) Notes make the public folder static/home page
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req,res) =>
+  res.sendFile(path.join(__dirname, '/notes.html')));
+
+// app.get('public/notes.html', (req,res)=> res.json());
 const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => console.log (`Server started on http://localhost:${PORT}`));
