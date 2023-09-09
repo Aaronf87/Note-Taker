6c01c28fe184598aa82217e130da1718fa6c564f
Aaron Flores <97806359+Aaronf87@users.noreply.github.com>
2023-09-09 01:29:44 -0500

    structured folders

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require ('express');
 // reminder this is used to deal with file paths!
 const path = require ('path');
-// const notes = require ('public/notes.html')
+const notes = require ('public/notes.html')
 const app = express();
 
 
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req,res) =>
   res.sendFile(path.join(__dirname, '/notes.html')));
 
-// app.get('public/notes.html', (req,res)=> res.json());
+app.get('public/notes.html', (req,res)=> res.json());
 const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => console.log (`Server started on http://localhost:${PORT}`));
