9d4ddb731ef16b2ba38bf8aad0f117eb793467d6
Aaron Flores <97806359+Aaronf87@users.noreply.github.com>
2023-09-10 11:53:36 -0500

    modified routes and gitignore file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require ('express');
 // reminder this is used to deal with file paths!
 const path = require ('path');
-const notes = require ('public/notes.html')
+// const notes = require ('public/notes.html')
 const app = express();
 
 
@@ -11,11 +11,34 @@ const app = express();
 // 1) Notes make the public folder static/home page
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req,res) =>
-  res.sendFile(path.join(__dirname, '/notes.html')));
+app.get('/notes', (req,res) =>{
 
-app.get('public/notes.html', (req,res)=> res.json());
-const PORT = process.env.PORT || 5002;
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+  
+});
+
+app.get('/api/notes', (req,res)=> {
+  
+  res.sendFile(path.join(__dirname, '/db/db.json'));
+  
+});
+
+//ROUTE FOR DEFAULT HOMEPAGE
+app.get('*', (req,res)=> {
+  
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// SET UP POST ROUTE
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+// unique id = uuid
+app.post('/api/notes', (req,res)=> {
+  console.log(req.body);
+  res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
+
+const PORT = process.env.PORT || 5003;
 
 app.listen(PORT, () => console.log (`Server started on http://localhost:${PORT}`));
 
